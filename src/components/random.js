cfcd1814ceecdbc33c66a0634aac52b020cdfc1b
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2019-01-01 22:56:23 +0700

    Random.

--- src/components/random.js
+++ src/components/random.js
@@ -1,11 +1,28 @@
 import React , { Component } from 'react'
+import styled from 'styled-components'
+import '../App.css'
 
+const RandomButton = styled.button`
+    width: 5%;
+    height: 40%;
+    background: blue;
+    color: yellow;
+    border-radius: 5px;
+`
 class Random extends Component {
-    constructor(props) {
+    constructor(props) { 
         super(props)
         this.state = {
-            counter: 0
+            counter: 0,
+            cardnum: 0
         }
+    }
+
+    randomCard() {
+        let randomNumber = Math.floor((Math.random()*10) + 1)
+        this.setState({
+            cardnum: randomNumber
+        })
     }
 
     addClick() {
@@ -24,8 +41,10 @@ class Random extends Component {
         return (
             <div>
                 <h1>Counter : {this.state.counter}</h1>
-                <div>
+                <h2>Number : {this.state.cardnum}</h2>
+                <div className="buttonna">
                     <button onClick={this.addClick.bind(this)}>+</button> 
+                    <RandomButton className="randombutton" onClick={this.randomCard.bind(this)}>Random</RandomButton>
                     <button onClick={this.removeClick.bind(this)}>-</button>
                 </div>
             </div>
